896cb647d74df05e324ccf5fe8f253a4e180db11
Greg Perkins <gperkins@roblox.com>
2023-04-07 10:45:21 -0700

    Build a readme

--- scripts/build.js
+++ scripts/build.js
@@ -1,20 +1,49 @@
 const fs = require("fs");
 const path = require("path");
+const { program } = require("commander");
+const mustache = require("mustache");
 
-const srcFilePath = path.join(__dirname, "..", "src", "bookmarklet.js");
-const distDirPath = path.join(__dirname, "..", "dist");
+program.option("-r, --readme", "generate README file with bookmarklet link");
+
+program.parse(process.argv);
+const options = program.opts();
+
+const rootDir = path.join(__dirname, "..");
+const srcDirPath = path.join(rootDir, "src");
+const srcFilePath = path.join(srcDirPath, "bookmarklet.js");
+const distDirPath = path.join(rootDir, "dist");
 const distFilePath = path.join(distDirPath, "bookmarklet.min.js");
 
 fs.promises
   .mkdir(distDirPath, { recursive: true })
   .then(() => fs.promises.readFile(srcFilePath, "utf-8"))
-  .then((data) => {
-    const bookmarklet = `javascript:${encodeURIComponent(data)}`;
+  .then((srcData) => {
+    const bookmarklet = `javascript:${encodeURIComponent(srcData)
+      .replace(/\(/g, "%28")
+      .replace(/\)/g, "%29")}`;
 
-    return fs.promises.writeFile(distFilePath, bookmarklet, "utf-8");
-  })
-  .then(() => {
-    console.log(`Generated bookmarklet at ${distFilePath}`);
+    if (options.readme) {
+      const readmeSrcFilePath = path.join(srcDirPath, "README-template.md");
+      const readmeDistFilePath = path.join(rootDir, "README.md");
+
+      const readmeData = {
+        BookmarkletLink: bookmarklet,
+      };
+
+      return fs.promises
+        .readFile(readmeSrcFilePath, "utf-8")
+        .then((template) => mustache.render(template, readmeData))
+        .then((readme) =>
+          fs.promises.writeFile(readmeDistFilePath, readme, "utf-8")
+        )
+        .then(() =>
+          console.log(`Generated README.md at ${readmeDistFilePath}`)
+        );
+    } else {
+      return fs.promises
+        .writeFile(distFilePath, bookmarklet, "utf-8")
+        .then(() => console.log(`Generated bookmarklet at ${distFilePath}`));
+    }
   })
   .catch((err) => {
     console.error(err);
---
+++ src/README-template.md
@@ -0,0 +1,19 @@
+# ChatGPT-Batea
+
+ChatGPT-Batea allows you to strip away all the extraneous stuff from your ChatGPT conversation window, so that you can save it as a clean single page screenshot (using Firefox's screenshot capability or something similar for other browsers) and share it with anyone.
+
+## Usage
+
+To use the bookmarklet, simply drag the following link to your bookmarks bar:
+
+[Bookmarklet]({{ BookmarkletLink }})
+
+## Building
+
+To build the bookmarklet, run the following command:
+
+```npm run build```
+
+To generate a `README.md` file with an embedded link to the bookmarklet, run the following command instead:
+
+```npm run build-readme```
